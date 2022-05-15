8bf5ded050b9f0496d77d98f1d684579f9f85afe
dragon9458 <dragon.whitee@qq.com>
2022-05-15 15:30:22 +0800

    init

---
+++ src/main/java/com/dragon/futu/futuapi/FutuapiApplication.java
@@ -0,0 +1,13 @@
+package com.dragon.futu.futuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutuapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutuapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dragon/futu/futuapi/FutuapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragon.futu.futuapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutuapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
